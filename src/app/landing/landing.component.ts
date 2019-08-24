22e2ad6b31d431a42240cadc413191b3dcc0be42
Roicel Laya <rlaya@autentia.cl>
2019-08-24 13:00:00 -0400

    Added user form validations.

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  userForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.userForm = new FormGroup({
+      'rut': new FormControl(null, Validators.required),
+      'cellphone': new FormControl(null, Validators.required),
+      'email': new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.userForm.value);
   }
 
 }
